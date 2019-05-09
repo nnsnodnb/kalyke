6289a994f8e4b0e0debcd77cfd0bb3832fceca26
nnsnodnb <ahr63_gej@me.com>
2019-05-09 22:39:58 +0900

    Remove client test...

--- tests/test_apns_client.py
+++
@@ -1,9 +0,0 @@
-from kalyke.client import APNsClient
-from unittest import TestCase
-
-
-class TestAPNsClient(TestCase):
-
-    def setUp(self):
-        self.client = APNsClient(team_id='TEAM_ID', auth_key_id='AUTH_KEY_ID', auth_key_filepath='',
-                                 bundle_id='com.example.app', use_sandbox=True)
