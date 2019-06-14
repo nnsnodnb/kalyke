9ec74ab2e9fa7d62ea4b3624e570d623e7e4dcc5
nnsnodnb <ahr63_gej@me.com>
2019-06-14 16:01:25 +0900

    Update examples

--- examples/apns.py
+++ examples/apns.py
@@ -1,8 +1,5 @@
 from kalyke.client import APNsClient
 from kalyke.payload import PayloadAlert, Payload
-from random import random
-
-import hashlib
 
 
 payload_alert = PayloadAlert(title='YOUR TITLE', body='YOUR BODY')
@@ -15,13 +12,14 @@ client = APNsClient(
 
 # Send single push notification
 
-registration_id = hashlib.sha256(('%.12f' % random()).encode('utf-8')).hexdigest()
+registration_id = 'a8a799ba6c21e0795b07b577b562b8537418570c0fb8f7a64dca5a86a5a3b500'
 
 result = client.send_message(registration_id, alert)
 
 # Send multiple push notifications
 registration_ids = [
-    hashlib.sha256(('%.12f' % random()).encode('utf-8')).hexdigest() for _ in range(10)
+    '87b0a5ab7b91dce26ea2c97466f7b3b82b5dda4441003a2d8782fffd76515b73',
+    '22a1b20cb67a43da4a8f006176788aa20271ac2e3ac0da0375ae3dc1db0de210'
 ]
 
 results = client.send_bulk_message(registration_ids, alert)
--- examples/voip.py
+++ examples/voip.py
@@ -1,7 +1,4 @@
 from kalyke.client import VoIPClient
-from random import random
-
-import hashlib
 
 
 client = VoIPClient(
@@ -15,14 +12,15 @@ alert = {
 
 # Send single VoIP notification
 
-registration_id = hashlib.sha256(('%.12f' % random()).encode('utf-8')).hexdigest()
+registration_id = 'a8a799ba6c21e0795b07b577b562b8537418570c0fb8f7a64dca5a86a5a3b500'
 
 result = client.send_message(registration_id, alert)
 
 # Send multiple VoIP notifications
 
 registration_ids = [
-    hashlib.sha256(('%.12f' % random()).encode('utf-8')).hexdigest() for _ in range(10)
+    '87b0a5ab7b91dce26ea2c97466f7b3b82b5dda4441003a2d8782fffd76515b73',
+    '22a1b20cb67a43da4a8f006176788aa20271ac2e3ac0da0375ae3dc1db0de210'
 ]
 
 results = client.send_bulk_message(registration_ids, alert)
