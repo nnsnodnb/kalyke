5a67657b05cfb1f98ebcf8ef92158ef8f9ffbac6
nnsnodnb <nnsnodnb@gmail.com>
2024-01-12 15:48:20 +0900

    Apply dataclass for ApnsConfig

--- kalyke/models/apns_config.py
+++ kalyke/models/apns_config.py
@@ -1,5 +1,6 @@
 import time
 import uuid
+from dataclasses import dataclass, field
 from typing import Dict, Optional
 
 import httpx
@@ -8,47 +9,28 @@ from .apns_priority import ApnsPriority
 from .apns_push_type import ApnsPushType
 
 
+@dataclass(frozen=True)
 class ApnsConfig:
     topic: str
-    push_type: ApnsPushType
-    identifier: Optional[str]
-    expiration: int
-    priority: int
-    collapse_id: Optional[str]
+    push_type: ApnsPushType = field(default=ApnsPushType.ALERT)
+    identifier: Optional[str] = field(default=None)
+    priority: ApnsPriority = field(default=ApnsPriority.IMMEDIATELY)
+    expiration: int = field(default=int(time.time()) + 2592000)
+    collapse_id: Optional[str] = field(default=None)
 
-    def __init__(
-        self,
-        topic: str,
-        push_type: ApnsPushType = ApnsPushType.ALERT,
-        identifier: Optional[str] = None,
-        expiration: Optional[int] = None,
-        priority: int = 10,
-        collapse_id: Optional[str] = None,
-    ) -> None:
-        self.topic = topic
-        self.push_type = push_type
-        if identifier:
-            self._valid_identifier(identifier=identifier)
-            self.identifier = identifier
-        else:
-            self.identifier = None
-        self.expiration = expiration if expiration is not None else int(time.time()) + 2592000
-        self.priority = ApnsPriority(priority).value
-        self.collapse_id = collapse_id
-
-    @staticmethod
-    def _valid_identifier(identifier) -> None:
-        try:
-            _ = uuid.UUID(hex=identifier, version=4)
-        except ValueError:
-            raise ValueError(f"{identifier} is invalid format.")
+    def __post_init__(self):
+        if self.identifier:
+            try:
+                _ = uuid.UUID(hex=self.identifier, version=4)
+            except ValueError:
+                raise ValueError(f"{self.identifier} is invalid format.")
 
     def make_headers(self) -> Dict[str, str]:
         headers: Dict[str, Optional[str]] = {
             "apns-push-type": self.push_type.value,
             "apns-id": self.identifier,
             "apns-expiration": str(self.expiration),
-            "apns-priority": str(self.priority),
+            "apns-priority": str(self.priority.value),
             "apns-topic": self.topic,
             "apns-collapse-id": self.collapse_id,
             "user-agent": f"python-httpx/{httpx.__version__} {self.topic}",
