b73faa0bc1f01076031148b8ffd8a63d665b5fe5
nnsnodnb <nnsnodnb@gmail.com>
2024-01-24 09:18:26 +0900

    Support cer & key are separated, including password

--- kalyke/clients/voip.py
+++ kalyke/clients/voip.py
@@ -1,3 +1,4 @@
+import warnings
 from dataclasses import dataclass, field
 from pathlib import Path
 from typing import Any, Dict, Optional, Union
@@ -5,6 +6,7 @@ from typing import Any, Dict, Optional, Union
 import httpx
 from httpx import AsyncClient
 
+from .._types import CertTypes
 from ..models import VoIPApnsConfig
 from . import __Client as BaseClient
 
@@ -15,6 +17,10 @@ class VoIPClient(BaseClient):
     auth_key_filepath: Union[str, Path]
     key_filepath: Optional[Union[str, Path]] = field(default=None)
     password: Optional[str] = field(default=None)
+
+    def __post_init__(self) -> None:
+        if self.key_filepath is None and self.password is not None:
+            warnings.warn("password is ignored because key_filepath is None.", UserWarning)
 
     async def send_message(
         self,
@@ -30,7 +36,13 @@ class VoIPClient(BaseClient):
 
     def _init_client(self, apns_config: VoIPApnsConfig) -> AsyncClient:
         headers = apns_config.make_headers()
-        context = httpx.create_ssl_context()
+
+        cert: CertTypes = (
+            self._get_auth_key_filepath().as_posix(),
+            self.key_filepath.as_posix() if self.key_filepath is not None else None,
+            self.password if self.key_filepath is not None else None,
+        )
+        context = httpx.create_ssl_context(cert=cert)
         context.load_cert_chain(self._get_auth_key_filepath())
         client = AsyncClient(headers=headers, verify=context, http2=True)
         return client
