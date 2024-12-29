694174a6e9b115940919918fc4e8ed4832a2a7d9
nnsnodnb <nnsnodnb@gmail.com>
2024-12-29 17:45:42 +0900

    Fix mypy typing errors

--- kalyke/models/apns_priority.py
+++ kalyke/models/apns_priority.py
@@ -2,6 +2,6 @@ from enum import Enum
 
 
 class ApnsPriority(Enum):
-    IMMEDIATELY: int = 10
-    BASED_ON_POWER: int = 5
-    POWER_CONSIDERATIONS_OVER_ALL_OTHER_FACTORS: int = 1
+    IMMEDIATELY = 10
+    BASED_ON_POWER = 5
+    POWER_CONSIDERATIONS_OVER_ALL_OTHER_FACTORS = 1
